c2ade03ad8d9306b8b4eb56e5086778e0fa23338
Manuel Guzman <manuguzman8@gmail.com>
2024-09-08 22:57:34 -0600

    First commit, adding project to Git

---
+++ pages/ciudades_index.vue
@@ -0,0 +1,24 @@
+<script>
+  export default {
+    async asyncData({ $content }) {
+      const books = await $content('ciudades').fetch()
+      return {
+        books
+      }
+    }
+  }
+</script>
+
+<template>
+  <div class="container">
+   <HeaderView />
+   <h3 style="margin-top: 15px">Información de Ciudades</h3>
+   <p>Esta sección presenta la información sobre las ciudades</p>
+   <ul>
+     <li v-for="book in books" :key="book.slug">
+       <NuxtLink :to="{ name: 'books-slug', params: { slug: book.slug } }">{{book.name}}</NuxtLink>
+     </li>
+   </ul>
+   <FooterView />
+ </div>
+</template>
---
+++ pages/edificios_index.vue
@@ -0,0 +1,24 @@
+<script>
+  export default {
+    async asyncData({ $content }) {
+      const books = await $content('edificios').fetch()
+      return {
+        books
+      }
+    }
+  }
+</script>
+
+<template>
+  <div class="container">
+   <HeaderView />
+   <h3 style="margin-top: 15px">Información de Edificios</h3>
+   <p>Esta sección presenta la información sobre los edificios</p>
+   <ul>
+     <li v-for="book in books" :key="book.slug">
+       <NuxtLink :to="{ name: 'books-slug', params: { slug: book.slug } }">{{book.name}}</NuxtLink>
+     </li>
+   </ul>
+   <FooterView />
+ </div>
+</template>
